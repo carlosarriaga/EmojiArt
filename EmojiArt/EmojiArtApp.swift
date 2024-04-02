ddebbf11fc7fbb51e0d3926aa91771feef795619
carlos.arriaga <carlos.arriaga@live.com>
2024-04-02 10:33:36 -0600

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Carlos Arriaga on 02/04/24.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
